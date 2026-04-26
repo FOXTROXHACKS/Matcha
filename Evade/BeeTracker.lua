2c76ab4006ed039e3ecfdd21df47d28e3790a5f1
Al3xanderrr_ <orestestosta@gmail.com>
2026-04-25 23:02:46 -0600

    Update BeeTracker.lua

--- Evade/BeeTracker.lua
+++ Evade/BeeTracker.lua
@@ -1,11 +1,11 @@
---_G.SafeZone = true
+--_G.NPC_SafeZone = true
 _G.TP_Cooldown = 0.05
 
 local SafeZoneCD = 0.1 
 local FixedSafePos = Vector3.new(-7.570, 380.103, 86.898)
 
 local player = game.Players.LocalPlayer
-local siguiendoABee = false
+local SiguiendoNPC = false
 local enZonaSegura = false
 
 print("------------------------------------------")
@@ -21,36 +21,36 @@ task.spawn(function()
         local playersFolder = gameFolder and gameFolder:FindFirstChild("Players")
         
         if hrp then
-            local bee = playersFolder and playersFolder:FindFirstChild("Bee")
-            local beeHRP = bee and bee:FindFirstChild("HumanoidRootPart")
+            local NPC = playersFolder and playersFolder:FindFirstChild("Bee")
+            local NPCHRP = NPC and NPC:FindFirstChild("HumanoidRootPart")
 
-            if beeHRP then
-                if not siguiendoABee then
+            if NPCHRP then
+                if not SiguiendoNPC then
                     print("--- BEE found! teleporting..")
-                    siguiendoABee = true
+                    SiguiendoNPC = true
                     enZonaSegura = false
                 end
                 pcall(function()
-                    hrp.Position = beeHRP.Position + Vector3.new(0, 2, 0)
+                    hrp.Position = NPCHRP.Position + Vector3.new(0, 2, 0)
                 end)
             else
-                if _G.BeeSafeZone  then
+                if _G.NPC_SafeZone  then
                     if not enZonaSegura then
                         print("--- BEE not found, returning to Fixed Safe Zone...")
                         enZonaSegura = true
-                        siguiendoABee = false
+                        SiguiendoNPC = false
                     end
                     hrp.Position = FixedSafePos
                     hrp.Velocity = Vector3.new(0, 0.2, 0)
                 else
-                    if siguiendoABee or enZonaSegura then
+                    if SiguiendoNPC or enZonaSegura then
                         print("--- BEE not found, SafeZone is OFF. Standing still.")
-                        siguiendoABee = false
+                        SiguiendoNPC = false
                         enZonaSegura = false
                     end
                 end
             end
         end
-        task.wait(siguiendoABee and _G.TP_Cooldown or SafeZoneCD)
+        task.wait(SiguiendoNPC and _G.TP_Cooldown or SafeZoneCD)
     end
 end)
