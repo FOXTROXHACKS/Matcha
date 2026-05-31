7f0b7747fa35b3fc8f42ccf52cd0fdc49968c402
Al3xanderrr_ <orestestosta@gmail.com>
2026-05-31 13:44:55 -0600

    Update EventAF.lua v 1.2 summer update

--- Duels/EventAF.lua
+++ Duels/EventAF.lua
@@ -9,7 +9,7 @@ local spawnablesFolder = workspace.Spawnables.SpawnablesClient
 local lastPadTP = 0
 
 UI.AddTab("Duels", function(tab)
-    local sec = tab:Section("Duels Autofarm - V1.1", "Left")
+    local sec = tab:Section("Duels Autofarm - V1.2", "Left")
     sec:Text("Settings")
     sec:Toggle("duels_toggle", "Enable AutoFarm", Duels_Autofarm, function(state)
         Duels_Autofarm = state
