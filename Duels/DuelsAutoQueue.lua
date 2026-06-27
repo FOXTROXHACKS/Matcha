28401f7d3ddd509d5196d73ed520870185c15130
Al3xanderrr_ <orestestosta@gmail.com>
2026-06-26 19:58:31 -0600

    Update DuelsAutoQueue.lua

--- Duels/DuelsAutoQueue.lua
+++ Duels/DuelsAutoQueue.lua
@@ -10,7 +10,7 @@ task.wait(2)
     local PadWaitTime = 3
     --local PadPath = workspace.PadZones.PadZone5.Pad1.Pad
     local player = game:GetService("Players").LocalPlayer
-    local spawnablesFolder = workspace.Spawnables.SpawnablesClient
+    local spawnablesFolder = workspace.SpawnablesClient
     local lastPadTP = 0
     
     -- UI Setup
