c8f2321f2ae4260fc68df46f14493804d71a984d
Al3xanderrr_ <orestestosta@gmail.com>
2026-07-11 14:00:55 -0600

    Update Tween_Service.lua

--- MISC/Tween_Service.lua
+++ MISC/Tween_Service.lua
@@ -1,9 +1,17 @@
 --[[
 _G.TweenService = loadstring(game:HttpGet("https://raw.githubusercontent.com/FOXTROXHACKS/Matcha/refs/heads/main/MISC/Tween_Service.lua"))()
+
+local hrp = game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart")
+local EndPoint = CFrame.new(0, 100, 0)
+
+local tween = _G.TweenService:Create(hrp, _G.TweenService.TweenInfo.new(1), {CFrame = EndPoint})
+tween:Play()
+
+OR
+
 local function MoveTo(EndPoint)
     local hrp = game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart")
     if hrp and _G.TweenService then
-        -- Aquí usas la variable global en lugar de un TweenService local
         _G.TweenService:Create(hrp, TweenInfo.new(1), {CFrame = EndPoint}):Play()
     end
 end
@@ -711,4 +719,5 @@ function TweenService:Create(instance, tweenInfo, properties)
 end
 
 TweenService.TweenInfo = TweenInfo
-_G.TweenService = TweenService
+--_G.TweenService = TweenService
+return TweenService
